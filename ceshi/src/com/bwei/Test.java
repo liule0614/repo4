7c485df71ac0836742177f54d075032fa1a0f95f
lidian <lidian@qq.com>
2019-11-01 14:10:52 +0800

    来咱们提交一下

---
+++ ceshi/src/com/bwei/Test.java
@@ -0,0 +1,16 @@
+package com.bwei;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+    }
+
+}
