0f70ed69b23443c9d22934db81ede75306023f09
lidian <lidian@qq.com>
2019-11-01 14:11:46 +0800

    来咱们提交一下1

--- ceshi/src/com/bwei/Test.java
+++ ceshi/src/com/bwei/Test.java
@@ -12,5 +12,9 @@ public class Test {
         System.out.println("哈哈哈哈哈哈");
         System.out.println("哈哈哈哈哈哈");
     }
-
+    public void aaa(){
+        int i = 3;
+        i+=1;
+        System.out.println(i);
+    }
 }
