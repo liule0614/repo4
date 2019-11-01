ae5c9fa808342d253465f643dc8e26d2557d52e7
lidian <lidian@qq.com>
2019-11-01 14:26:49 +0800

    来我们再次修改一下

--- ceshi/src/com/bwei/Test.java
+++ ceshi/src/com/bwei/Test.java
@@ -11,10 +11,12 @@ public class Test {
         System.out.println("哈哈哈哈哈哈");
         System.out.println("哈哈哈哈哈哈");
         System.out.println("哈哈哈哈哈哈");
+        System.out.println("兹阿莱一次啊");
     }
     public void aaa(){
         int i = 3;
         i+=1;
+        int  c= 3;
         System.out.println(i);
     }
 }
